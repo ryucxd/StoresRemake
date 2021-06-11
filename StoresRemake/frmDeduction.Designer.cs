3abf273e7dc28275007e1129ef2b0675672fa98b
ryucxd <ryucxd@gmail.com>
2021-06-11 14:57:01 +0100

    Add project files.

---
+++ StoresRemake/frmDeduction.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace StoresRemake
+{
+    partial class frmDeduction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDeduct = new System.Windows.Forms.Button();
+            this.txtStockCode = new System.Windows.Forms.TextBox();
+            this.btnWipe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stock Deduction";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 316);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            // 
+            // btnDeduct
+            // 
+            this.btnDeduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeduct.Location = new System.Drawing.Point(677, 471);
+            this.btnDeduct.Name = "btnDeduct";
+            this.btnDeduct.Size = new System.Drawing.Size(127, 32);
+            this.btnDeduct.TabIndex = 3;
+            this.btnDeduct.Text = "Deduct Stock";
+            this.btnDeduct.UseVisualStyleBackColor = true;
+            this.btnDeduct.Click += new System.EventHandler(this.btnDeduct_Click);
+            // 
+            // txtStockCode
+            // 
+            this.txtStockCode.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtStockCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStockCode.Location = new System.Drawing.Point(272, 59);
+            this.txtStockCode.Name = "txtStockCode";
+            this.txtStockCode.Size = new System.Drawing.Size(273, 62);
+            this.txtStockCode.TabIndex = 0;
+            this.txtStockCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtStockCode_KeyDown);
+            // 
+            // btnWipe
+            // 
+            this.btnWipe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnWipe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWipe.Location = new System.Drawing.Point(12, 471);
+            this.btnWipe.Name = "btnWipe";
+            this.btnWipe.Size = new System.Drawing.Size(127, 32);
+            this.btnWipe.TabIndex = 4;
+            this.btnWipe.Text = "Wipe List";
+            this.btnWipe.UseVisualStyleBackColor = true;
+            this.btnWipe.Click += new System.EventHandler(this.btnWipe_Click);
+            // 
+            // frmDeduction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 515);
+            this.Controls.Add(this.btnWipe);
+            this.Controls.Add(this.btnDeduct);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStockCode);
+            this.Name = "frmDeduction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hardware Deduction";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDeduct;
+        private System.Windows.Forms.TextBox txtStockCode;
+        private System.Windows.Forms.Button btnWipe;
+    }
+}
+
---
+++ StoresRemake/frmDeduction.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StoresRemake
+{
+    public partial class frmDeduction : Form
+    {
+        public frmDeduction()
+        {
+            InitializeComponent();
+        }
+
+
+        private void addDeduction(string description, double quantity_in_stock)
+        {
+            //first we need to see if there are no rows
+            if (dataGridView1.Rows.Count == 0)
+            {
+                //make it from scratch
+                DataTable dt = new DataTable();
+                dt.Clear();
+                dt.Columns.Add("Stock Code");
+                dt.Columns.Add("Description");
+                dt.Columns.Add("Quantity in Stock");
+                dt.Columns.Add("Quantity to Deduct");
+
+                DataRow dataRow = dt.NewRow();
+                dataRow["Stock Code"] = txtStockCode.Text;
+                dataRow["Description"] = description;
+                dataRow["Quantity in Stock"] = quantity_in_stock;
+                dataRow["Quantity to Deduct"] = "1";
+                dt.Rows.Add(dataRow);
+
+                dataGridView1.DataSource = dt;
+            }
+            else
+            {
+                //else we make a new datatable and rebind it
+                DataTable dt = new DataTable();
+                dt.Clear();
+                dt.Columns.Add("Stock Code");
+                dt.Columns.Add("Description");
+                dt.Columns.Add("Quantity in Stock");
+                dt.Columns.Add("Quantity to Deduct");
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    DataRow dataRow = dt.NewRow();
+                    dataRow["Stock Code"] = row.Cells[0].Value.ToString();
+                    dataRow["Description"] = row.Cells[1].Value.ToString(); ;
+                    dataRow["Quantity in Stock"] = row.Cells[2].Value.ToString(); ;
+                    dataRow["Quantity to Deduct"] = row.Cells[3].Value.ToString();
+                    dt.Rows.Add(dataRow);
+                }
+
+                DataRow newRow = dt.NewRow();
+                newRow["Stock Code"] = txtStockCode.Text;
+                newRow["Description"] = description;
+                newRow["Quantity in Stock"] = quantity_in_stock;
+                newRow["Quantity to Deduct"] = "1";
+                dt.Rows.Add(newRow);
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void txtStockCode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //enter is pressed
+                //i think the codes get scanned in at once so doing this here will be fine i guess
+                string sql = "select [description] from dbo.stock where stock_code = " + txtStockCode.Text;
+                string description = "";
+                double quantity = 0;
+                using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        var getDescription = cmd.ExecuteScalar();
+                        if (getDescription == null)
+                            description = "No Description.";
+                        else
+                            description = getDescription.ToString();
+                    }
+                    sql = "select amount_in_stock FROM dbo.stock where stock_code = '" + txtStockCode.Text + "'";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        var getQty = cmd.ExecuteScalar();
+                        if (getQty != null)
+                            quantity = Convert.ToDouble(getQty);
+                    }
+                    conn.Close();
+                }
+
+                //load data into the dgv
+                if (description != "No Description.") //if theres no description then there is prob no stock code for this
+                {
+                    addDeduction(description, quantity);
+                    formatting();
+                }
+                txtStockCode.Text = "";
+                txtStockCode.Focus();
+            }
+        }
+
+        private void formatting()
+        {
+            try
+            {
+                dataGridView1.Columns.Remove("Remove");
+            }
+            catch
+            {
+
+            }
+            //also add the button here too
+
+
+            DataGridViewButtonColumn removeButtonColumn = new DataGridViewButtonColumn();
+            removeButtonColumn.Name = "Remove";
+            removeButtonColumn.Text = "✖";
+            removeButtonColumn.UseColumnTextForButtonValue = true;
+            int columnIndex = (dataGridView1.Columns.Count);
+            if (dataGridView1.Columns["Remove"] == null)
+            {
+                dataGridView1.Columns.Insert(columnIndex, removeButtonColumn);
+            }
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                col.ReadOnly = true;
+
+            }
+            int quantity = dataGridView1.Columns["Quantity to Deduct"].Index;
+            dataGridView1.Columns[quantity].ReadOnly = false;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.ActiveControl = txtStockCode;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView1.Columns["Remove"].Index;
+            if (e.ColumnIndex == index)
+            {
+                try
+                {
+                    dataGridView1.Columns.Remove("Remove");
+                }
+                catch
+                {
+
+                }
+                //else we make a new datatable and rebind it   
+                DataTable dt = new DataTable();
+                dt.Clear();
+                dt.Columns.Add("Stock Code");
+                dt.Columns.Add("Description");
+                dt.Columns.Add("Quantity in Stock");
+                dt.Columns.Add("Quantity to Deduct");
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (e.RowIndex == row.Index)  
+                        continue;
+                    DataRow dataRow = dt.NewRow();
+                    dataRow["Stock Code"] = row.Cells[0].Value.ToString();
+                    dataRow["Description"] = row.Cells[1].Value.ToString(); ;
+                    dataRow["Quantity in Stock"] = row.Cells[2].Value.ToString(); ;
+                    dataRow["Quantity to Deduct"] = row.Cells[3].Value.ToString();
+                    dt.Rows.Add(dataRow);
+                }
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dt;
+                formatting();
+            }
+        }
+
+        private void btnDeduct_Click(object sender, EventArgs e)
+        {
+            //here we add the entry to stock deduction log and remove the quantity from dbo.stock
+            //first get the indexes of the columns because this is pretty important and it would suck if it removed the stock code instead of quantity xd
+            int stockIndex = 0, QuantityIndex = 3; //the default values that they should be
+            stockIndex = dataGridView1.Columns["Stock Code"].Index;
+            QuantityIndex = dataGridView1.Columns["Quantity to Deduct"].Index;
+            string sql = "";
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToInt32(row.Cells[QuantityIndex].Value) == 0)
+                    continue; //skip anything with 0 
+                using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+                {
+                    conn.Open();
+                    sql = "insert into dbo.stock_deduction_log (stock_code,date_deducted, quantity_deducted) VALUES ('" + row.Cells[stockIndex].Value.ToString() + "',GETDATE()," + row.Cells[QuantityIndex].Value.ToString() + ")";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        //insert into stock deduction log 
+                        cmd.ExecuteNonQuery();
+                    }
+                    //run here
+                    sql = "UPDATE dbo.stock SET amount_in_stock = amount_in_stock - " + row.Cells[QuantityIndex].Value.ToString() + " WHERE stock_code = '" + row.Cells[stockIndex].Value.ToString() + "'";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                }
+            }
+
+            //after this remove anyhting that has a quantity to deduct that is > 0
+            int index = dataGridView1.Columns["Remove"].Index;
+            try
+            {
+                dataGridView1.Columns.Remove("Remove");
+            }
+            catch
+            {
+
+            }
+
+            //else we make a new datatable and rebind it
+            DataTable dt = new DataTable();
+            dt.Clear();
+            dt.Columns.Add("Stock Code");
+            dt.Columns.Add("Description");
+            dt.Columns.Add("Quantity in Stock");
+            dt.Columns.Add("Quantity to Deduct");
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToDouble(row.Cells[QuantityIndex].Value) == 0)
+                {
+
+                    DataRow dataRow = dt.NewRow();
+                    dataRow["Stock Code"] = row.Cells[0].Value.ToString();
+                    dataRow["Description"] = row.Cells[1].Value.ToString(); ;
+                    dataRow["Quantity in Stock"] = row.Cells[2].Value.ToString(); ;
+                    dataRow["Quantity to Deduct"] = row.Cells[3].Value.ToString();
+                    dt.Rows.Add(dataRow);
+                }
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dt;
+            formatting();
+
+            if (dataGridView1.Rows.Count > 0)
+                MessageBox.Show("Items with 0 quantity have not been deducted or removed from the table. Please correct these values!"); //same messagebox that was in the old version
+            txtStockCode.Focus();
+
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e) //this handles the entry of non digits etc into the last column 
+        { 
+            e.Control.KeyPress -= new KeyPressEventHandler(quantityCol_KeyPress);
+            if (dataGridView1.CurrentCell.ColumnIndex == 3) //qty
+            {
+                TextBox tb = e.Control as TextBox;
+                if (tb != null)
+                {
+                    tb.KeyPress += new KeyPressEventHandler(quantityCol_KeyPress); 
+                }
+            }
+        }
+
+        private void quantityCol_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.CurrentCell = null;
+            dataGridView1.ClearSelection();
+            this.ActiveControl = txtStockCode;
+        }
+
+        private void btnWipe_Click(object sender, EventArgs e)  //wipes everything from the list 
+        {
+            int index = dataGridView1.Columns["Remove"].Index; //removes button before it breaks the next stock code
+           try
+            {
+                dataGridView1.Columns.Remove("Remove");
+            }
+            catch
+            {
+                //dont need to add anything here
+            }
+            dataGridView1.DataSource = null;
+        }
+    }
+}
